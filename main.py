53a0f06436fa822061193024f7d609091e60ea09
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-11-16 22:20:42 +0330

    adding  edition

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from turtle import Turtle, Screen
+from paddlle import Paddle
+
 
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor("#000000")
 screen.tracer(1)
 screen.title("Pong")
-# creating the middle line
+
+# Implementing paddles
+player1_paddle = Paddle(300, 0)
+player1_paddle.createPaddle()
+
+player2_paddle = Paddle(-300, 0)
+player2_paddle.createPaddle()
+
+# Implementing the middle line
 for y_position in range(-300, 300, 20):
 	square = Turtle("square")
 	square.shapesize(0.4, 0.4)
@@ -13,6 +23,8 @@ for y_position in range(-300, 300, 20):
 	square.color("white")
 	square.penup()
 	square.goto(0, y_position)
+
+
 
 screen.mainloop()
 
